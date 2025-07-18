0d7237818f3ec147ab208c7a3fe563e54c274f48
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2025-07-18 14:48:17 +0000

    migrate to postgresql

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 try {
     global $connexion;
-    $connexion = new PDO("mysql:host=localhost;dbname=bibliotheque;charset=utf8", "root", "");
+    $connexion = new PDO("pgsql:host=dpg-d1t5s1c9c44c73d651ug-a;dbname=bibliotheque_hvs4;charset=utf8", "root", "mp8Dv1m4Pf5Pjd8DnXELSgU24iFksN8b");
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
