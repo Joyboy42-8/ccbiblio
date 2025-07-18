4f6dad731bd9c9db921b0e2032914a57f60feb22
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2025-07-18 11:59:35 +0000

    Add font awesome icons

--- views/additionnals/footer.php
+++ views/additionnals/footer.php
@@ -1,6 +1,7 @@
     <footer class="bg-blue-900 fixed bottom-0 w-full text-center p-5 text-white hover:opacity-90">
         &copy; <?= date("Y") ?> Crafters Company
     </footer>
+    <script src="https://kit.fontawesome.com/7880d5522c.js" crossorigin="anonymous"></script>
     </body>
 
     </html>
